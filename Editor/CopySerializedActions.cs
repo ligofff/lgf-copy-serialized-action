b241d8c2f0ecd89bd421b2fbafee0d69a15e365e
ligofff <liga@ligofff.ru>
2023-12-02 09:13:14 +0300

    Package initialization

---
+++ Editor/CopySerializedActions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+
+public static class CopySerializedActions
+{
+    static SerializedObject _operationSource;
+
+    [MenuItem("CONTEXT/Component/Copy Serialized Values")]
+    public static void CopySerializedFromBase(MenuCommand command)
+    {
+        _operationSource = new SerializedObject(command.context);
+    }
+
+    [MenuItem("CONTEXT/Component/Paste Serialized Values")]
+    public static void PasteSerializedFromBase(MenuCommand command)
+    {
+        SerializedObject dest = new SerializedObject(command.context);
+        
+        SerializedProperty propertyIterator = _operationSource.GetIterator();
+        
+        if (propertyIterator.NextVisible(true))
+        {
+            while (propertyIterator.NextVisible(true))
+            {
+                SerializedProperty property = dest.FindProperty(propertyIterator.name); 
+
+                if (property != null && property.propertyType == propertyIterator.propertyType) 
+                {
+                    dest.CopyFromSerializedProperty(propertyIterator); 
+                }
+            }
+        }
+        
+        dest.ApplyModifiedProperties();
+        
+        EditorUtility.SetDirty(dest.targetObject);
+    }
+}
